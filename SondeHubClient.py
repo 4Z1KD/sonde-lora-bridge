c4d79e02e20c4c4834904d66b87f58034505fd10
gilifon <gilifon@gmail.com>
2025-12-27 20:00:13 +0200

    remove freq validation from the client

--- SondeHubClient.py
+++ SondeHubClient.py
@@ -141,9 +141,6 @@ class SondeHubClient:
         for field in telemetry_fields:
             if field in mapped_packet:
                 telemetry[field] = mapped_packet[field]
-        # consider removing as this was fixed in SondeLoraBridge.py
-        if "frequency" in telemetry and "MHZ" in telemetry["frequency"].upper():
-                telemetry["frequency"] = float(telemetry["frequency"].replace("MHz", ""))
 
         # Uploader information
         if self.uploader_position:
